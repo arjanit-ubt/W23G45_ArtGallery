647a30b0e18d58f313cb2ffd37814df875273a59
Arjanit Islami <ai36087@ubt-uni.net>
2023-11-11 11:08:05 +0000

    Deleted W23G45_Art_Gallery

--- W23G45_Art_Gallery/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace W23G45_Art_Gallery.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
